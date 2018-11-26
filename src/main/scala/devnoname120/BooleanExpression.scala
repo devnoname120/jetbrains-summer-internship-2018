cf36af79e8c5a2514113525e5f940ef0b5131f94
devnoname120 <devnoname120+gh@gmail.com>
2018-11-26 21:15:32 +0100

    BooleanExpression: Add documentation

--- src/main/scala/devnoname120/BooleanExpression.scala
+++ src/main/scala/devnoname120/BooleanExpression.scala
@@ -1,5 +1,6 @@
 package devnoname120
 
+import play.api.libs.json.JsonConfiguration.Aux
 import play.api.libs.json._
 
 sealed trait BooleanExpression
@@ -10,28 +11,48 @@ case class Not(e: BooleanExpression) extends BooleanExpression
 case class Or(e1: BooleanExpression, e2: BooleanExpression) extends BooleanExpression
 case class And(e1: BooleanExpression, e2: BooleanExpression) extends BooleanExpression
 
+
+/**
+  * Play JSON Read/Write implicits
+  */
 trait BooleanExpressionJsonFormat {
   // Only keep the class name in the JSON type field
-  implicit val cfg = JsonConfiguration(
+  implicit val cfg: Aux[Json.MacroOptions] = JsonConfiguration(
     typeNaming = JsonNaming { fullName =>
       Unit
       fullName.split('.').last
     }
   )
 
-  implicit val trueFormat = Json.format[True.type]
-  implicit val falseFormat = Json.format[False.type]
-  implicit val variableFormat = Json.format[Variable]
-  implicit lazy val notFormat = Json.format[Not]
-  implicit lazy val orFormat = Json.format[Or]
-  implicit lazy val andFormat = Json.format[And]
-  implicit val booleanExpresssionFormat: OFormat[BooleanExpression] = Json.format[BooleanExpression]
+  implicit val trueFormat: OFormat[True.type] = Json.format[True.type]
+  implicit val falseFormat: OFormat[False.type] = Json.format[False.type]
+  implicit val variableFormat: OFormat[Variable] = Json.format[Variable]
+  implicit lazy val notFormat: OFormat[Not] = Json.format[Not]
+  implicit lazy val orFormat: OFormat[Or] = Json.format[Or]
+  implicit lazy val andFormat: OFormat[And] = Json.format[And]
+  implicit val booleanExpressionFormat: OFormat[BooleanExpression] = Json.format[BooleanExpression]
 }
 
+/**
+  * Convert a BooleanExpression from/to JSON
+  */
 object BooleanExpression extends BooleanExpressionJsonFormat {
+  /**
+    * Parse a [[BooleanExpression]] from a JSON string
+    *
+    * @param booleanExpression A JSON string containing a boolean expression
+    * @return a [[BooleanExpression]] parsed from the string
+    */
   def fromJSON(booleanExpression: String) : BooleanExpression = {
     Json.parse(booleanExpression).as[BooleanExpression]
   }
+
+  /**
+    * Convert a [[BooleanExpression]] to a JSON string
+    *
+    * @param booleanExpression A boolean expression
+    * @return the string representation
+    */
   def toJSON(booleanExpression: BooleanExpression) : String = {
     Json.toJson(booleanExpression).toString
   }
