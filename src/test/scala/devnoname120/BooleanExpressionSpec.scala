bc921350ec7d4adf0f94d32ed465cc98b983d5e4
devnoname120 <devnoname120+gh@gmail.com>
2018-11-30 23:12:26 +0100

    BooleanExpression: Add tests about conversion to JSON

--- src/test/scala/devnoname120/BooleanExpressionSpec.scala
+++ src/test/scala/devnoname120/BooleanExpressionSpec.scala
@@ -1,15 +1,10 @@
 package devnoname120
 
+import devnoname120.Extensions._
 import org.scalatest._
-import play.api.libs.json.JsResultException
-import Extensions._
+import play.api.libs.json.{JsResultException, Json}
 
 class BooleanExpressionSpec extends FlatSpec with Matchers {
-  "Variable" should "not allow to have an empty symbol" in {
-    a [IllegalArgumentException] should be thrownBy {
-      Variable("")
-    }
-  }
 
   "BooleanExpression" should "be converted to a math string correctly" in {
     True.toMathString() shouldEqual "true"
@@ -21,71 +16,104 @@ class BooleanExpressionSpec extends FlatSpec with Matchers {
     Variable("long variable").toMathString() shouldEqual "long_variable"
   }
 
-  "True JSON" should "be parsed correctly" in {
+
+  "True" should "be parsed correctly from JSON" in {
     val expr = BooleanExpression.fromJSON("""{"_type": "True"}""")
     expr shouldEqual True
   }
-  "False JSON" should "be parsed correctly" in {
+  it should "be converted correctly to JSON" in {
+    Json.parse(True.toJSON) shouldEqual Json.obj("_type" -> "True")
+  }
+
+  "False" should "be parsed correctly from JSON" in {
     val expr = BooleanExpression.fromJSON("""{"_type": "False"}""")
     expr shouldEqual False
   }
+  it should "be converted correctly to JSON" in {
+    Json.parse(False.toJSON) shouldEqual Json.obj("_type" -> "False")
+  }
 
-  "Variable JSON" should "be parsed correctly" in {
+  "Variable" should "not allow to have an empty symbol" in {
+    a [IllegalArgumentException] should be thrownBy {
+      Variable("")
+    }
+  }
+  it should "be parsed correctly from JSON" in {
     val expr = BooleanExpression.fromJSON("""{"_type": "Variable", "symbol": "foobar"}""")
     expr shouldEqual Variable("foobar")
   }
-  it should "throw JsResultException if symbol is missing" in {
+  it should "throw JsResultException if symbol is missing when parsing from JSON" in {
     the[JsResultException] thrownBy {
       BooleanExpression.fromJSON("""{"_type": "Variable"}""")
     } should have message "JsResultException(errors:List((/symbol,List(JsonValidationError(List(error.path.missing),WrappedArray())))))"
   }
-  it should "throw JsResultException if symbol is empty" in {
+  it should "throw JsResultException if symbol is empty when parsing from JSON" in {
     the[JsResultException] thrownBy {
       BooleanExpression.fromJSON("""{"_type": "Variable", "symbol": ""}""")
     } should have message "JsResultException(errors:List((/symbol,List(JsonValidationError(List(error.minLength),WrappedArray(1))))))"
   }
-  it should "throw JsResultException if symbol is not a string" in {
+  it should "throw JsResultException if symbol is not a string when parsing from JSON" in {
     the[JsResultException] thrownBy {
       BooleanExpression.fromJSON("""{"_type": "Variable", "symbol": true}""")
     } should have message "JsResultException(errors:List((/symbol,List(JsonValidationError(List(error.expected.jsstring),WrappedArray())))))"
   }
+  it should "be converted correctly to JSON" in {
+    Json.parse(Variable("foobar").toJSON) shouldEqual Json.obj("_type" -> "Variable", "symbol" -> "foobar")
+  }
 
-  "Not JSON" should "be parsed correctly" in {
+  "Not" should "be parsed correctly from JSON" in {
     val expr = BooleanExpression.fromJSON("""{"_type": "Not", "e": {"_type": "True"}}""")
     expr shouldEqual Not(True)
   }
-  it should "use last instance of redefined parameter" in {
+  it should "use last instance of redefined parameter when parsing from JSON" in {
     val expr = BooleanExpression.fromJSON("""{"_type": "Not", "e": {"_type": "False"}, "e": {"_type": "True"}}""")
     expr shouldEqual Not(True)
     val expr2 = BooleanExpression.fromJSON("""{"_type": "Not", "e": {"_type": "True"}, "e": {"_type": "False"}}""")
     expr2 shouldEqual Not(False)
   }
-  it should "throw JsResultException if expression is missing" in {
+  it should "throw JsResultException if expression is missing from JSON" in {
     the[JsResultException] thrownBy {
       BooleanExpression.fromJSON("""{"_type": "Not"}""")
     } should have message "JsResultException(errors:List((/e,List(JsonValidationError(List(error.path.missing),WrappedArray())))))"
   }
+  it should "be converted correctly to JSON" in {
+    Json.parse(Not(True).toJSON) shouldEqual Json.obj("_type" -> "Not", "e" -> Json.parse(True.toJSON))
+  }
 
-  "Or JSON" should "be parsed correctly" in {
+  "Or" should "be parsed correctly from JSON" in {
     val expr1 = BooleanExpression.fromJSON("""{"_type": "Or", "e1": {"_type": "True"}, "e2": {"_type": "False"}}""")
     expr1 shouldEqual Or(True, False)
     val expr2 = BooleanExpression.fromJSON("""{"_type": "Or", "e2": {"_type": "False"}, "e1": {"_type": "True"}}""")
     expr2 shouldEqual Or(True, False)
   }
-  it should "throw JsResultException if first expression is missing" in {
+  it should "throw JsResultException if first expression is missing from JSON" in {
     the[JsResultException] thrownBy {
       BooleanExpression.fromJSON("""{"_type": "Or", "e2": {"_type": "False"}}""")
     } should have message "JsResultException(errors:List((/e1,List(JsonValidationError(List(error.path.missing),WrappedArray())))))"
   }
-  it should "throw JsResultException if second expression is missing" in {
+  it should "throw JsResultException if second expression is missing from JSON" in {
     the[JsResultException] thrownBy {
       BooleanExpression.fromJSON("""{"_type": "Or", "e1": {"_type": "True"}}""")
     } should have message "JsResultException(errors:List((/e2,List(JsonValidationError(List(error.path.missing),WrappedArray())))))"
   }
+  it should "be converted correctly to JSON" in {
+    Json.parse(Or(True, False).toJSON) shouldEqual Json.obj(
+      "_type" -> "Or",
+      "e1" -> Json.parse(True.toJSON),
+      "e2" -> Json.parse(False.toJSON)
+    )
+  }
 
-  "And JSON" should "be parsed correctly" in {
+  "And" should "be parsed correctly from JSON" in {
     val expr = BooleanExpression.fromJSON("""{"_type": "And", "e1": {"_type": "True"}, "e2": {"_type": "False"}}""")
     expr shouldEqual And(True, False)
+  }
+  it should "be converted correctly to JSON" in {
+    Json.parse(Or(False, True).toJSON) shouldEqual Json.obj(
+      "_type" -> "Or",
+      "e1" -> Json.parse(False.toJSON),
+      "e2" -> Json.parse(True.toJSON)
+    )
   }
 
   "Unknown operator JSON" should "throw JsResultException" in {
